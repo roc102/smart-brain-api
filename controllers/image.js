3375894bb4596ccd5a71935bfb7ecabe76ad0ea5
roc102 <kyroc102@gmail.com>
2023-05-28 01:15:02 +0530

    adding files

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '7a32d401e4cc48f095352ed129ea47e7' 
+});
+
+
+const handleApiCall = (req, res) => {
+
+  app.models.predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
