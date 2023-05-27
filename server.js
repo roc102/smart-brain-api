26a2674e4eb4f5b22c1070cfdb42a402b47f89e1
roc102 <kyroc102@gmail.com>
2023-05-28 01:31:36 +0530

    fix

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db= knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1', 
-      user : 'postgres', 
-      password : 'password', 
+      user : '', 
+      password : '', 
       database : 'smart_brain' 
     }
 });
